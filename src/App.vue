1f0a5e7d580c758c7d849746e818e84b36177229
JulioSalazar12 <u202017572@upc.edu.pe>
2021-11-18 15:59:38 -0500

    feat: improved main component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <h1 class="mt-6 font-weight-bold mx-auto">APP FINANCE</h1>
+    <v-hover>
+      <template v-slot:default="{ hover }">
+        <v-card :class="`elevation-${hover ? 20 : 10}`" class="mx-auto my-5 transition-swing">
+          <v-tabs centered background-color="black" dark>
+            <v-tab to="/">User</v-tab>
+            <v-tab to="/About">Rate</v-tab>
+            <v-tab>Client</v-tab>
+            <v-tab>Financial Entity</v-tab>
+            <v-tab>Bills</v-tab>
+            <v-tab>Costs</v-tab>
+            <v-tab>Costs</v-tab>
+          </v-tabs>
+        </v-card>
+      </template>
+    </v-hover>
+
+    <v-main class="mx-6 my-3">
+      <router-view/>
+    </v-main>
+
+  </v-app>
 </template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'App',
+}
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: Montserrat;
+  background-color: #f5f5f5;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
