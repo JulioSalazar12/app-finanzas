0b022398a0775756d91366b328ed6ddb244f1ddd
JulioSalazar12 <u202017572@upc.edu.pe>
2021-11-22 03:25:00 -0500

    feat: pago-letra component completed with errors by backend

--- src/pago-letra/pages/pago-letra.vue
+++ src/pago-letra/pages/pago-letra.vue
@@ -4,33 +4,34 @@
       <v-card-title class="pt-0">
         <strong>Create a Pago Letra</strong>
         <v-spacer></v-spacer>
-        <v-switch v-model="switch1" inset
-                  :label="`Mora: ${switch1.toString()}`"
+        <v-switch v-model="mora" inset
+                  :label="`Mora: ${mora.toString()}`"
                   color="indigo accent-4"
                   class="mt-0 pt-0"
                   hide-details
         ></v-switch>
       </v-card-title>
-      <v-form ref="form">
-        <v-combobox   class="mb-3" clearable outlined hide-details dense   v-model="entidadFinanciera" label="Entidad Financiera" :items="entidadesFinancieras"   ></v-combobox>
-        <v-combobox   class="mb-3" clearable outlined hide-details dense   v-model="letra" label="Letra" :items="letras"   ></v-combobox>
-        <v-text-field class="mb-3" dense outlined  clearable hide-details  v-model="montoPago" label="Monto Pago"      ></v-text-field>
-        <v-text-field class="mb-3" dense outlined  clearable hide-details  v-model="ic" label="Interes Compensatorio" v-if="switch1"     ></v-text-field>
-        <v-text-field class="mb-3" dense outlined  clearable hide-details  v-model="im" label="Interes Moratorio"     v-if="switch1" ></v-text-field>
-        <v-text-field class="mb-3" dense outlined  clearable hide-details  v-model="tCEPm" label="tCEPm"                               v-if="switch1"    ></v-text-field>
-        <v-text-field class="mb-3" dense outlined  clearable hide-details  v-model="valor_Entregado_Mora" label="Valo entregado mora"  v-if="switch1"    ></v-text-field>
-        <v-text-field class="mb-3" dense outlined  clearable hide-details  v-model="diasTranscurridos" label="Dias transcurridos"      ></v-text-field>
-        <v-text-field class="mb-3" dense  outlined  clearable hide-details v-model="diasTranscurridos"  label="Fecha pago"    placeholder="2019-02-03T00:00:00"></v-text-field>
+      <v-form ref="form"  v-model="valid" lazy-validation>
+        <v-combobox   class="mb-3" :rules="rules" clearable outlined hide-details dense   v-model="entidadFinanciera" label="Entidad Financiera" :items="entidadesFinancieras"   ></v-combobox>
+        <v-combobox   class="mb-3" :rules="rules" clearable outlined hide-details dense   v-model="letra" label="Letra" :items="letras"   ></v-combobox>
+        <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="montoPago" label="Monto Pago"   type="number"   ></v-text-field>
+        <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="ic" label="Interes Compensatorio" v-if="mora"     ></v-text-field>
+        <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="im" label="Interes Moratorio"     v-if="mora" ></v-text-field>
+        <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="tCEPm" label="tCEPm"                               v-if="mora"    ></v-text-field>
+        <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="valor_Entregado_Mora" label="Valo entregado mora" type="number" v-if="mora"    ></v-text-field>
+        <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="diasMora" label="Dias de Mora" type="number"     ></v-text-field>
+        <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="diasTranscurridos" label="Dias transcurridos" type="number"     ></v-text-field>
+        <v-text-field class="mb-3" :rules="rules" dense  outlined  clearable hide-details v-model="fechaPago"  label="Fecha pago"    placeholder="2019-02-03T00:00:00"></v-text-field>
         <v-divider class="mt-5 mb-3"></v-divider>
         <v-card-actions>
           <v-btn outlined color="red" @click="reset">Cancel</v-btn>
           <v-spacer></v-spacer>
-          <v-btn outlined color="indigo accent-4" @click="createNewPagoLetra">Submit</v-btn>
+          <v-btn outlined color="indigo accent-4" :disabled="!valid" @click="createNewPagoLetra">Submit</v-btn>
         </v-card-actions>
       </v-form>
     </v-card>
-    <v-snackbar v-model="snackbar" dark>
-      {{ text }}
+    <v-snackbar v-model="snackbar" color="success" dark>
+      Pago de letra creado exitosamente
       <template v-slot:action="{ attrs }">
         <v-btn text v-bind="attrs" @click="snackbar = false">
           Close
@@ -41,33 +42,89 @@
 </template>
 
 <script>
+import PagoLetraService from '@/pago-letra/services/pago-letra.service'
+
 export default {
   name: "pago-letra",
   data: () => ({
+    valid: true,
     snackbar: false,
-    text: '',
     entidadesFinancieras: [],
     letras: [],
-    switch1: false,
-    im: '',
-    ic: '',
+    mora: false,
+    im: 0,
+    ic: 0,
     entidadFinanciera: '',
     letra: '',
     montoPago: '',
-    tCEPm: '',
-    valor_Entregado_Mora: '',
+    tCEPm: 0,
+    valor_Entregado_Mora: 0,
     diasTranscurridos: '',
-    fecha: '',
+    diasMora: '',
+    fechaPago: '',
+    rules: [
+      v => !!v || 'This field is required',
+    ],
   }),
+  mounted() {
+    this.refreshListLetras();
+    this.refreshListEntidades();
+  },
   methods: {
     createNewPagoLetra(){
-      //TODO: enviar pago letra.
-      console.log("newPageLetter");
+      if(this.$refs.form.validate()) {
+        let charLetra = this.letra[0];
+        let charEF = this.entidadFinanciera[0];
+        const PagoLetra = {
+          ic: this.ic,
+          im: this.im,
+          monto_Pago: this.montoPago,
+          mora: this.mora,
+          tCEPm:  this.tCEPm,
+          valor_Entregado_Mora: this.valor_Entregado_Mora,
+          dias_de_mora: this.diasMora,
+          dias_transcurridos: this.diasTranscurridos,
+          idEf:       { id: charEF[0]},
+          idLetra:    { id: charLetra[0]},
+          fecha_Pago: null
+        };
+        PagoLetraService.createNewPagoLetra(PagoLetra)
+          .then((response) => {
+            console.log("new Pago: ", response.data);
+          })
+        this.snackbar = true;
+        this.reset();
+      }
+
     },
     reset () {
-      this.text = 'Formulario borrado';
-      this.snackbar = true;
       this.$refs.form.reset()
+    },
+    refreshListEntidades () {
+      PagoLetraService.getAllEntidadesFinancieras()
+          .then((response) => {
+            this.entidadesFinancieras = response.data.map(this.displayEntidadesName)
+            console.log(response.data);
+            console.log("entidades:",this.entidadesFinancieras);
+          })
+    },
+    displayEntidadesName(item){
+      return [
+        `${item.id} - ${item.nombre}`
+      ]
+    },
+    refreshListLetras () {
+      PagoLetraService.getAllLetras()
+          .then((response) => {
+            this.letras = response.data.map(this.displayLetras)
+            console.log(response.data);
+            console.log("Letras: ", this.letras);
+          })
+    },
+    displayLetras(item){
+      return [
+        `${item.id} - Letra`
+      ]
     }
   }
 }
